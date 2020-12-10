824773d10831ab7d48696eef9e51469d83d0a7bd
Philipp Gualdi <philipp.gualdi@tugraz.at>
2020-12-10 09:37:52 +0100

    test test script

--- run-tests.sh
+++ run-tests.sh
@@ -7,10 +7,11 @@
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-pydocstyle invenio_marc21 tests docs && \
-check-manifest --ignore ".travis-*" && \
-python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
+pydocstyle invenio_marc21 tests docs
+check-manifest --ignore ".*-requirements.txt*"
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
 docker-services-cli --verbose up es postgresql redis
 python -m pytest
 tests_exit_code=$?
+docker-services-cli down
 exit "$tests_exit_code" 
