cbc3132b320803ae0bc50e2ac57c93ac454d53e1
Philipp Gualdi <philipp.gualdi@tugraz.at>
2020-12-10 08:21:31 +0100

    test githubaction yml

--- run-tests.sh
+++ run-tests.sh
@@ -7,12 +7,10 @@
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-docker-services-cli up postgresql es redis && \
 pydocstyle invenio_marc21 tests docs && \
 check-manifest --ignore ".travis-*" && \
 python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
-docker-services-cli up es postgresql redis
+docker-services-cli --verbose up es postgresql redis
 python -m pytest
 tests_exit_code=$?
-docker-services-cli down
 exit "$tests_exit_code" 
